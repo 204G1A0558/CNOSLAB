bd32e28445960d30af0b8664e40f5e8210ab261d
204G1A0558 <94547165+204G1A0558@users.noreply.github.com>
2022-08-30 11:33:48 +0530

    Sample.C
    S.Faraz i have completed my program using C

---
+++ OSLAB/Sample.C
@@ -0,0 +1,10 @@
+/* Name Of the Candidate: S. Faraz
+   Roll No: 204G1A0558
+   Title of the Experiment: sample program using c
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
